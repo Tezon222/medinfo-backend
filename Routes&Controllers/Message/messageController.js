229ff849d6871e4025ce3ae11c141225b7240789
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2024-08-26 00:14:32 +0100

    Sending messages and database structure completed

--- Routes&Controllers/Message/messageController.js
+++ Routes&Controllers/Message/messageController.js
@@ -2,23 +2,31 @@ const Messages = require('../../Model/Messages/messageSchema')
 const Conversation = require("../../Model/Messages/conversationSchema")
 
 const sendMessage = async(req, res) =>{
-    const {senderId} = req.params
-    const {sender,receiverId, message} = req.body 
-
-    if(!message || !sender || receiverId || senderId){
-        res.status(400).json({message: "Please enter valid "})
-    }
-        const message_new = await Messages.create({senderId, sender: sender === "Doctor" ? "Doctor" : "Patient", receiverId, message, receiver: sender === "Doctor" ? "Patient" : "Doctor"})
-        const conversationExisit = await Conversation.findOne({participants: {$all : [senderId, receiverId]}})
-
-        if(!conversationExisit){
-            const new_conversation = await Conversation.create({participants:[senderId, receiverId]})
-            new_conversation.messageIds.push(message_new._id)
-            new_conversation.save()
+    try {
+        const {senderId} = req.params
+        const {sender,receiverId, message} = req.body 
+    
+        if(!message || !sender || !receiverId || !senderId){
+            res.status(400).json({message: "Please enter valid message"})
         }else{
-            conversationExisit.messageIds.push(message_new._id)
-            await conversationExisit.save()
+            const message_new = await Messages.create({senderId, sender: sender === "Doctor" ? "Doctor" : "Patient", receiverId, message, receiver: sender === "Doctor" ? "Patient" : "Doctor"})
+            const conversationExisit = await Conversation.findOne({participants: {$all : [senderId, receiverId]}})
+    
+            if(!conversationExisit){
+                const new_conversation = await Conversation.create({participants:[senderId, receiverId]})
+                new_conversation.messageIds.push(message_new._id)
+                new_conversation.save()
+                res.status(200).json({message:"Message sent conversation does not exists"})
+            }else if(conversationExisit){
+                conversationExisit.messageIds.push(message_new._id)
+                await conversationExisit.save()
+                res.status(200).json({message:"Message sent conversation exists"})
+            }
         }
+    } catch (error) {
+        throw new Error(error.error)
+        // console.log(error)
+    }
 } 
 
 module.exports = {sendMessage}
--- Routes&Controllers/Message/messageRoute.js
+++ Routes&Controllers/Message/messageRoute.js
@@ -1,8 +1,7 @@
 const express = require("express")
 const route = express.Router()
+const {sendMessage} = require('./messageController')
 
-route.get('/send', (req,res)=>{
-    res.send("send messages")
-})
+route.post('/send/:senderId', sendMessage)
 
 module.exports = route
