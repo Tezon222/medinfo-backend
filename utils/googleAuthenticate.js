3d188e5a73800a2d8e2568b3534d93f0662ee59b
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2024-09-15 22:39:42 +0100

    added gender to schema

--- utils/googleAuthenticate.js
+++ utils/googleAuthenticate.js
@@ -1,21 +1,39 @@
 const passport = require("passport")
 const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 const Patient = require("../Model/Users/patientSchema")
+const { callApi } = require("@zayne-labs/callapi")
+
 passport.use(new GoogleStrategy({
     clientID:     process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: process.env.GOOGLE_AUTH_CALLBACKURL,
     passReqToCallback   : true
   },
-  async function verify(request, accessToken, refreshToken, profile, done) {
+  async function verify(request, accessToken, refreshToken, profile,done) {
         const {id, family_name, email, given_name, picture } = profile
         const existingUser = await Patient.findOne({googleId: id})
         const existingUseremail = await Patient.findOne({email})
         if(existingUser || existingUseremail){
          return  done(null, existingUseremail)
         } else{
-          const user = await Patient.create({firstName: family_name, googleId: id, email, picture, lastName: given_name, verified: true,gender: 'Male' })
-          return done(null, user)
+          const { data, error } = await callApi("https://people.googleapis.com/v1/people/me", {
+            method: "GET",
+            auth:{
+              bearer:accessToken
+            },
+            query:{
+              key: process.env.GOOGLE_AUTH_APIKEY,
+              personFields: "genders,birthdays"
+            }
+          })
+          if(error == null){
+            const gender = data?.genders[0].formattedValue
+            const user = await Patient.create({firstName: family_name, googleId: id, email, picture, lastName: given_name, verified: true,gender })
+            return done(null, user)
+          }else{
+            return done(error, null)
+          }
+          
         }
   }
 ));
