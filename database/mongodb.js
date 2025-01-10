64c3fdf9edcf09aa57eadb1ac1681bdd127e7a39
Max <maximilianogbuabor@gmail.com>
2025-01-10 22:26:49 +0100

    Resolve file case sensitivity issue

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose")
+import mongoose from "mongoose"
 
 const connectdb = async() =>{
     try{
@@ -9,4 +9,5 @@ const connectdb = async() =>{
     }
 
 }
-module.exports = connectdb
+ 
+export default connectdb
